48d754b03e1e12c9cf69909be29ea5ddd5aa2510
HKNCLK0 <celikhakan5255@gmail.com>
2022-01-04 03:27:37 +0300

    Responsive

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -2,6 +2,6 @@ import Header from "./Header";
 import Modals from "./Modals";
 import Loader from "./Loader";
 import Footer from "./Footer";
-import Error from "./Error";
+import Error from "../Login/Error";
 
 export { Header, Modals, Loader, Footer, Error };
