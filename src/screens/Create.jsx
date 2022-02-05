7bfb052dd87f5650b33c818b900c60418ca8688a
HKNCLK0 <celikhakan5255@gmail.com>
2022-02-06 00:54:27 +0300

    Country Bug Fixed

--- src/screens/Create.jsx
+++ src/screens/Create.jsx
@@ -93,6 +93,8 @@ const Create = () => {
       });
   };
 
+  console.log(data);
+
   return (
     <>
       <Header />
@@ -100,113 +102,117 @@ const Create = () => {
         <Box className="w-11/12 md:w-4/5">
           <h1 className="font-bold md:text-xl text-textColor">Oluştur</h1>
           <Error error={error}>{errorMessage}</Error>
-          <div className="flex flex-col md:flex-row gap-8 md:gap-x-12 w-full px-4 md:px-16">
-            <img
-              src={data.businessImage}
-              alt="businessImage"
-              className="w-full md:w-1/3"
-            />
-            <div className="flex flex-col gap-4">
-              <h1 className="text-textColor text-lg font-semibold">
-                {data.businessName}
-              </h1>
-              <h1 className="text-textColor text-sm">
-                {data.businessCategory}
-              </h1>
-              <p className="text-textColor pt-4 text-sm">
-                {data.businessAddress} <br />{" "}
-                {data.businessIlce
-                  ? data.businessIlce[0] +
-                    data.businessIlce.substr(1).toLowerCase()
-                  : ""}{" "}
-                /{" "}
-                {data.businessCountry
-                  ? City[data.businessCountry - 1].name
-                  : ""}
-              </p>
-              <div className="flex flex-col gap-6">
-                <div className="flex flex-col gap-2">
-                  <h1 className="text-textColor font-semibold text-sm">
-                    Randevu Tarihini Seçiniz
-                  </h1>
-                  {data.businessMeetDates ? (
-                    <div className="grid grid-cols-3 md:grid-cols-6 gap-x-4">
-                      {data.businessMeetDates.map((meetDate, index) => (
-                        <button
-                          key={index}
-                          onClick={() =>
-                            setSelecetedDate(
-                              meetDate === selectedDate ? "" : meetDate
-                            )
-                          }
-                          className={`${
-                            selectedDate === meetDate
-                              ? "border-transparent bg-textColor text-boxColor"
-                              : "text-textColor border-borderAndOtherRed "
-                          } border-2 font-semibold px-2 py-2 rounded-lg text-sm transition-colors duration-200 hover:bg-textColor hover:text-boxColor hover:border-transparent`}
-                        >
-                          {meetDate}
-                        </button>
-                      ))}
-                    </div>
-                  ) : (
-                    <Loader />
-                  )}
-                </div>
-                <div className="flex flex-col gap-2">
-                  <h1 className="text-textColor font-semibold text-sm">
-                    Randevu Saatini Seçiniz
-                  </h1>
-                  {selectedDate.length > 0 ? (
-                    data.businessMeetTimes ? (
-                      <div className="grid grid-cols-3 md:grid-cols-6 gap-x-4 gap-y-2">
-                        {data.businessMeetTimes.map((meetTime, index) => {
-                          return (
-                            <button
-                              key={index}
-                              disabled={doluSaat.find((saat) =>
-                                saat.date == selectedDate
-                                  ? saat.clock == meetTime
-                                    ? true
-                                    : false
-                                  : null
-                              )}
-                              onClick={() =>
-                                setSelectedTime(
-                                  meetTime === selectedTime ? "" : meetTime
-                                )
-                              }
-                              className={`${
-                                selectedTime === meetTime
-                                  ? "border-transparent bg-textColor text-boxColor"
-                                  : "text-textColor border-borderAndOtherRed"
-                              } border-2 font-semibold disabled:bg-disabledColor disabled:border-transparent disabled:text-textColor disabled:cursor-not-allowed px-2 py-2 rounded-lg text-sm transition-colors duration-200 hover:bg-textColor hover:text-boxColor hover:border-transparent`}
-                            >
-                              {meetTime}
-                            </button>
-                          );
-                        })}
+          {data ? (
+            <div className="flex flex-col md:flex-row gap-8 md:gap-x-12 w-full px-4 md:px-16">
+              <img
+                src={data.businessImage}
+                alt="businessImage"
+                className="w-full md:w-1/3"
+              />
+              <div className="flex flex-col gap-4">
+                <h1 className="text-textColor text-lg font-semibold">
+                  {data.businessName}
+                </h1>
+                <h1 className="text-textColor text-sm">
+                  {data.businessCategory}
+                </h1>
+                <p className="text-textColor pt-4 text-sm">
+                  {data.businessAddress} <br />{" "}
+                  {data.businessIlce
+                    ? data.businessIlce[0] +
+                      data.businessIlce.substr(1).toLowerCase()
+                    : ""}{" "}
+                  /{" "}
+                  {data.businessCountry
+                    ? City.find((x) => x.name == data.businessCountry).name
+                    : null}
+                </p>
+                <div className="flex flex-col gap-6">
+                  <div className="flex flex-col gap-2">
+                    <h1 className="text-textColor font-semibold text-sm">
+                      Randevu Tarihini Seçiniz
+                    </h1>
+                    {data.businessMeetDates ? (
+                      <div className="grid grid-cols-3 md:grid-cols-6 gap-x-4">
+                        {data.businessMeetDates.map((meetDate, index) => (
+                          <button
+                            key={index}
+                            onClick={() =>
+                              setSelecetedDate(
+                                meetDate === selectedDate ? "" : meetDate
+                              )
+                            }
+                            className={`${
+                              selectedDate === meetDate
+                                ? "border-transparent bg-textColor text-boxColor"
+                                : "text-textColor border-borderAndOtherRed "
+                            } border-2 font-semibold px-2 py-2 rounded-lg text-sm transition-colors duration-200 hover:bg-textColor hover:text-boxColor hover:border-transparent`}
+                          >
+                            {meetDate}
+                          </button>
+                        ))}
                       </div>
                     ) : (
                       <Loader />
-                    )
-                  ) : (
-                    <h1 className="text-textColor font-semibold">
-                      Lütfen Önce Randevu Tarihi Seçiniz
+                    )}
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <h1 className="text-textColor font-semibold text-sm">
+                      Randevu Saatini Seçiniz
                     </h1>
-                  )}
+                    {selectedDate.length > 0 ? (
+                      data.businessMeetTimes ? (
+                        <div className="grid grid-cols-3 md:grid-cols-6 gap-x-4 gap-y-2">
+                          {data.businessMeetTimes.map((meetTime, index) => {
+                            return (
+                              <button
+                                key={index}
+                                disabled={doluSaat.find((saat) =>
+                                  saat.date == selectedDate
+                                    ? saat.clock == meetTime
+                                      ? true
+                                      : false
+                                    : null
+                                )}
+                                onClick={() =>
+                                  setSelectedTime(
+                                    meetTime === selectedTime ? "" : meetTime
+                                  )
+                                }
+                                className={`${
+                                  selectedTime === meetTime
+                                    ? "border-transparent bg-textColor text-boxColor"
+                                    : "text-textColor border-borderAndOtherRed"
+                                } border-2 font-semibold disabled:bg-disabledColor disabled:border-transparent disabled:text-textColor disabled:cursor-not-allowed px-2 py-2 rounded-lg text-sm transition-colors duration-200 hover:bg-textColor hover:text-boxColor hover:border-transparent`}
+                              >
+                                {meetTime}
+                              </button>
+                            );
+                          })}
+                        </div>
+                      ) : (
+                        <Loader />
+                      )
+                    ) : (
+                      <h1 className="text-textColor font-semibold">
+                        Lütfen Önce Randevu Tarihi Seçiniz
+                      </h1>
+                    )}
+                  </div>
+                </div>
+                <div className="w-full flex justify-center pt-4">
+                  <Button
+                    onClick={handleCreate}
+                    disabled={!selectedTime || !selectedDate}
+                  >
+                    Randevu Oluştur
+                  </Button>
                 </div>
               </div>
-              <div className="w-full flex justify-center pt-4">
-                <Button
-                  onClick={handleCreate}
-                  disabled={!selectedTime || !selectedDate}
-                >
-                  Randevu Oluştur
-                </Button>
-              </div>
             </div>
-          </div>
+          ) : (
+            <Loader />
+          )}
         </Box>
         {/* TODO:Yorum Ekleme Yapılacak */}
         <Box className="w-11/12 md:w-4/5 md:px-32 px-6">
